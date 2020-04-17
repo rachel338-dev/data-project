da92e55f5f40d6402e1d07775bc6b52be572fea9
Rachel Owusu-Ansah <rachel.owusuansah@shopify.com>
2020-04-17 15:41:50 -0400

    update

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,17 @@
 require_relative 'boot'
+
 require 'rails/all'
 require 'rack'
 require 'rack/cors'
+
 Bundler.require(*Rails.groups)
+
 module DataBlogProject
   class Application < Rails::Application
     config.load_defaults 6.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3001'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
